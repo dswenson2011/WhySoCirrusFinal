034dc6d42c027e3a86fe1798cabbd3709a59509c
dswenson2011 <dswenson2011@gmail.com>
2015-07-13 13:22:22 -0400

    Start of datastore API

--- app/scripts/core.js
+++ app/scripts/core.js
@@ -1,18 +1,13 @@
 (function () {
 	var app = angular.module('app.core', ['ngMaterial', 'ngMdIcons', 'ngMessages', 'ngRoute', 'btford.socket-io']);
-
 	app.config(function ($mdThemingProvider) {
 		$mdThemingProvider.theme('default')
 			.primaryPalette('teal')
 			.accentPalette('blue-grey');
 	});
-
-	// [TODO:David] rework socket factory as we might need to do forwards?
 	app.factory('socket', function (socketFactory) {
 		return socketFactory();
 	});
-
-	// Toast hooks for notifications
 	app.run(['$mdToast', '$rootScope', 'socket', function ($mdToast, $rootScope, socket) {
 		socket.on('notification', function (data) {
 			$mdToast.show(toast(data));
@@ -27,7 +22,6 @@
 			});
 		};
 	}]);
-
 	app.service('observer', function () {
 		var observer = this;
 		var observerCallbacks = [];
