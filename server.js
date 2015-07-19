489984c072118a3a3f2866737e9fcb002b8240bd
dswenson2011 <dswenson2011@gmail.com>
2015-07-19 04:23:35 -0400

    HOLY VM BATMAN

--- server.js
+++ server.js
@@ -15,14 +15,53 @@ server.socket.on('connection', function () {
 
 server.app.use(require('express').static(__dirname + '/app'));
 
+server.app.get('/VMs', function (req, res) {
+	require('edge')
+		.func('ps', function () {
+			/*
+			$credential = New-Object System.Management.Automation.PsCredential("domware\Administrator", (ConvertTo-SecureString "W3ntw0rth@boston" -AsPlainText -Force))
+			$s = New-PSSession -ComputerName peter.dell.whysocirr.us -Credential $credential
+			function a(){Get-VM|ConvertTo-JSON}
+			Invoke-Command -Session $s -ScriptBlock ${function:a}
+			Remove-PSSession $s
+			*/
+		})('', function (error, result) {
+			console.log(error);
+			console.log(result);
+			res.json(JSON.parse(result));
+		}, true);
+});
+
+server.app.get('/test', function (req, res) {
+	// { 'path': 'C:\\VHDS\\Test10.vhdx', 'name': 'Test10', 'ram': '2147483648','hardDrive': '8589934592'}
+	var object = { 'path': 'C:\\VHDS\\' + req.query.name + '.vhdx', 'name': '' + req.query.name + '', 'ram': '2147483648', 'hardDrive': '8589934592' };
+	console.log(object);
+	require('edge')
+		.func('ps', function () {
+			/*
+			$credential = New-Object System.Management.Automation.PsCredential("domware\Administrator", (ConvertTo-SecureString "W3ntw0rth@boston" -AsPlainText -Force))
+			$s = New-PSSession -ComputerName peter.dell.whysocirr.us -Credential $credential
+			$object =  ${inputFromJS} | ConvertFrom-Json
+			$object
+			function a($input){$input = $input | ConvertTo-Json | ConvertFrom-Json; New-VM –Name $input.name –MemoryStartupBytes $input.ram –NewVHDPath $input.path -NewVHDSizeBytes $input.hardDrive | ConvertTo-JSON}
+			Invoke-Command -Session $s -ScriptBlock ${function:a} -inputObject ${object}
+			Remove-PSSession $s
+			*/
+		})(JSON.stringify(object), function (error, result) {
+			console.log(error);
+			console.log(result);
+			res.json(JSON.parse(result));
+		}, true);
+});
+
 server.app.post('/githubHook', function (req, res) {
-	var ps = require('edge').func('ps', function(){
+	var ps = require('edge').func('ps', function () {
 		/*
 			cd C:\Users\Administrator\Desktop\Node\WhySoCirrusFinal
 			git pull
 		*/
 	});
-	ps('',function(err, result){
+	ps('', function (err, result) {
 		//console.log(result);
 	});
 	res.sendStatus(200);
