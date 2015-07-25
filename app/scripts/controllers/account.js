af9439ba555ca81592363a6387ce0ea822207a4e
David Swenson <dswenson2011@gmail.com>
2015-07-25 13:46:59 -0400

    Removed account logs spam to prepare for database integration

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -14,29 +14,7 @@
 			return index >= (AccountCtrl.query.limit * (AccountCtrl.query.page - 1));
 		};
 		AccountCtrl.logs = [
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 3', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' },
-			{ action: 'Create', item: 'Test VM 2', date: 'Mon Sep 28 1999 14:36:22 GMT-0700', result: 'OK' }
+			{ action: 'Create', item: 'Test VM', date: 'Mon Sep 28 1998 14:36:22 GMT-0700', result: 'OK' }
 		];
 		layout.page('account');
 		layout.tools('');
